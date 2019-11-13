157a3ef89bba2d8d707ad0eb288fa0da8b2f5869
Andreas Kvist <andreas.kvist@bouvet.no>
2019-11-13 08:33:03 +0100

    Added define function

--- sap/ui/index.d.ts
+++ sap/ui/index.d.ts
@@ -4,3 +4,9 @@
 /// <reference path="./layout/index.d.ts" />
 /// <reference path="./model/index.d.ts" />
 /// <reference path="./unified/index.d.ts" />
+
+declare namespace sap {
+	namespace ui {
+		function define(modulePaths: string[], loadedModules: (...modules: sap.ui.base.Object[]) => void);
+	}
+}
