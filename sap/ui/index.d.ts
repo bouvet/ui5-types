be159eb68596fcb3278edaf8379bd0421464d632
Andreas Kvist <andreas.kvist@bouvet.no>
2019-11-15 16:40:30 +0100

    Simplified

--- sap/ui/index.d.ts
+++ sap/ui/index.d.ts
@@ -7,17 +7,6 @@
 
 declare namespace sap {
 	namespace ui {
-		interface Constructor<T extends sap.ui.base.Object = sap.ui.base.Object, U extends {} = {}> {
-			new (): T;
-			new (mSettings: U): T;
-			new (sId: string): T;
-			new (sId: string, mSettings: U): T;
-
-			extend(sClassName: string): Constructor<T, U>
-			extend(sClassName: string, oClassInfo: object): Constructor<T, U>
-			extend(sClassName: string, oClassInfo: object, FNMetaImpl: Function): Constructor<T, U>
-		}
-
-		function define<T extends typeof sap.ui.base.Object>(modulePaths: string[], loadedModules: (...modules: Array<T>) => void | Function | object): void;
+		function define<T>(modulePaths: string[], loadedModules: (...modules: T[]) => void | Function | object): void;
 	}
 }
