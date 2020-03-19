f795ec01b549012afc740fd42c1ad543ca835bf8
Andreas Kvist <andreas.kvist@bouvet.no>
2020-03-19 08:02:10 +0100

    getCore() and byId()

--- sap/ui.d.ts
+++ sap/ui.d.ts
@@ -1,5 +1,11 @@
 declare namespace sap {
 	namespace ui {
 		function define<T>(modulePaths: string[], loadedModules: (...modules: T[]) => void | Function | object): void;
+
+		interface Core {
+			byId<T extends sap.ui.core.Control>(id: string): T;
+		}
+		function getCore(): Core;
 	}
 }
+
