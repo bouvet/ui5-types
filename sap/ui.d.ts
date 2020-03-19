794c51f009d882e159a8f4eb002ce9b2b2b9553f
Andreas Kvist <andreas.kvist@bouvet.no>
2020-03-19 08:06:07 +0100

    set/get model

--- sap/ui.d.ts
+++ sap/ui.d.ts
@@ -4,6 +4,8 @@ declare namespace sap {
 
 		interface Core {
 			byId<T extends sap.ui.core.Control>(id: string): T;
+			getModel<T extends sap.ui.model.Model>(id: string): T;
+			setModel<T extends sap.ui.model.Model>(model: T, id: string): void;
 		}
 		function getCore(): Core;
 	}
