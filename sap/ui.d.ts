296f8b7b09f2ee2b384181ecf994822707645714
Andreas Kvist <ankvi@hotmail.com>
2020-10-02 13:52:51 +0200

    Added sap.ui.require method

--- sap/ui.d.ts
+++ sap/ui.d.ts
@@ -1,6 +1,7 @@
 declare namespace sap {
 	namespace ui {
 		function define<T>(modulePaths: string[], loadedModules: (...modules: T[]) => void | Function | object): void;
+		function require(modulePaths: string[], loadedModules: (...modules: unknown[]) => void | Function | object): void;
 
 		function getCore(): sap.ui.core.Core;
 	}
